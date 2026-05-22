4173b26a7a868f1ee0d6610fcb65094c9daa156f
Theon O. <garlicbredfries@gmail.com>
2026-05-22 03:25:16 -0700

    Add first puzzle, and other (empty) files

---
+++ checker.js
@@ -0,0 +1,12 @@
+function ansCheck(userAns,ans,closes) {
+    verdict = document.getElementById("verdict");
+    if (userAns === ans) {
+        verdict.innerHTML = "Correct!";
+    }
+    if (closes.includes(userAns)) {
+        verdict.innerHTML = "You're on the right track, but you're not there yet.";
+    }
+    else {
+        verdict.innerHTML += "Hmm... Not what I'm looking for.";
+    }
+}
---
+++ puzzles/puzz0001/main.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var userInput = document.getElementById("inputBox");
+    userInput.addEventListener("keydown", function(event) {
+        document.getElementById("verdict").innerHTML = "";
+        if (event.key === "Enter") {
+            var userAns = userInput.value.replace(/\s/g, '').toLowerCase();
+            ansCheck(userAns, "malamar", ["inkay"]);
+        }
+    });
+    document.getElementById("inputButton").addEventListener("click", function(event) {
+        var userAns = userInput.value.replace(/\s/g, '').toLowerCase();
+        ansCheck(userAns, "malamar", ["inkay"]);
+    });
+});
