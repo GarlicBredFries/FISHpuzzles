dd7777830bca594afccc61e5314875c7c7669030
Theon O. <garlicbredfries@gmail.com>
2026-05-22 20:47:48 -0700

    Reformatting

--- checker.js
+++ checker.js
@@ -1,3 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var userInput = document.getElementById("inputBox");
+    userInput.addEventListener("keydown", function(event) {
+        document.getElementById("verdict").innerHTML = "";
+        if (event.key === "Enter") {
+            var userAns = userInput.value.replace(/\s/g, '').toLowerCase();
+            ansCheck(userAns, answer, closes);
+        }
+    });
+    document.getElementById("inputButton").addEventListener("click", function(event) {
+        var userAns = userInput.value.replace(/\s/g, '').toLowerCase();
+        ansCheck(userAns, answer, closes);
+    });
+});
+
 function ansCheck(userAns,ans,closes) {
     verdict = document.getElementById("verdict");
     if (userAns === ans) {
